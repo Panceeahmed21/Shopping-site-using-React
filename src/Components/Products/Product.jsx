fb1154b16374c5019645ea7a7ff731759f6eda53
Pancee Ahmed <Panceeahmed22@gmail.com>
2023-10-20 13:40:32 +0200

    rating

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -23,21 +23,32 @@ const Product = () => {
         return (
           <div className="col-md-4">
             <Card height="400px">
-              <Card.Img variant="top" height="200"  src={prod.image} className="w-75 m-auto py-2 cardImg" />
+              <Card.Img
+                variant="top"
+                height="200"
+                src={prod.image}
+                className="w-75 m-auto py-2 cardImg"
+              />
               <Card.Body>
-                <Card.Title className="card_title h4" >  {prod.title.split(" ").slice(0, 8).join(" ")}...</Card.Title>
+                <Card.Title className="card_title h4">
+                  {" "}
+                  {prod.title.split(" ").slice(0, 8).join(" ")}...
+                </Card.Title>
                 <Card.Text className="text-muted">
                   {prod.description.split(" ").slice(0, 10).join(" ")}...
                 </Card.Text>
 
-                <Rating c
+                <Rating
+                  c
                   value={prod.rating.rate}
                   edit={false}
                   size={24}
                   activeColor="#ffe529"
                 />
                 <Link to={`/products/${prod.id}`}>
-                  <span className="text-dark fw-bold btn bg-success text-white">Show Details</span>
+                  <span className="text-dark fw-bold btn bg-success text-white">
+                    Show Details
+                  </span>
                 </Link>
               </Card.Body>
             </Card>
