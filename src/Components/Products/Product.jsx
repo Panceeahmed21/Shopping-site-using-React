82c98841403bc8b93513edeb577848cc01dbedb5
Pancee Ahmed <Panceeahmed22@gmail.com>
2023-10-21 18:27:20 +0200

    update product hook

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import {} from "./Product.css";
 import Rating from "react-rating-stars-component";
-import Loading from "../Loading";
+import Loading from "../../Loading/Loading";
 import useProductsData from "../../Hooks/useProductsData";
 
 const Product = () => {
@@ -11,48 +11,48 @@ const Product = () => {
     "https://fakestoreapi.com/products"
   );
 
-  return <Loading loading={isLoading} error ={error}  >
-    {
-  Products.map((prod) => {
-    return (
-      <>
-        <div className="col-md-4">
-          <Card height="400px">
-            <Card.Img
-              variant="top"
-              height="200"
-              src={prod.image}
-              className="w-75 m-auto py-2 cardImg"
-            />
-            <Card.Body>
-              <Card.Title className="card_title h4">
-                {" "}
-                {prod.title.split(" ").slice(0, 8).join(" ")}...
-              </Card.Title>
-              <Card.Text className="text-muted">
-                {prod.description.split(" ").slice(0, 10).join(" ")}...
-              </Card.Text>
+  return (
+    <Loading loading={isLoading} error={error}>
+      {Products.map((prod) => {
+        return (
+          <>
+            <div className="col-md-4">
+              <Card height="400px">
+                <Card.Img
+                  variant="top"
+                  height="200"
+                  src={prod.image}
+                  className="w-75 m-auto py-2 cardImg"
+                />
+                <Card.Body>
+                  <Card.Title className="card_title h4">
+                    {" "}
+                    {prod.title.split(" ").slice(0, 8).join(" ")}...
+                  </Card.Title>
+                  <Card.Text className="text-muted">
+                    {prod.description.split(" ").slice(0, 10).join(" ")}...
+                  </Card.Text>
 
-              <Rating
-                c
-                value={prod.rating.rate}
-                edit={false}
-                size={24}
-                activeColor="#ffe529"
-              />
-              <Link to={`/products/${prod.id}`}>
-                <span className="text-dark fw-bold btn bg-success text-white">
-                  Show Details
-                </span>
-              </Link>
-            </Card.Body>
-          </Card>
-        </div>
-      </>
-    );
-  })
-    }
-  </Loading>;
+                  <Rating
+                    c
+                    value={prod.rating.rate}
+                    edit={false}
+                    size={24}
+                    activeColor="#ffe529"
+                  />
+                  <Link to={`/products/${prod.id}`}>
+                    <span className="text-dark fw-bold btn bg-success text-white">
+                      Show Details
+                    </span>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </div>
+          </>
+        );
+      })}
+    </Loading>
+  );
 };
 
 export default Product;
